60840cd426d67d43ccd3de0fc631b64582aec770
uttam-web <uttam.influxiq@gmail.com>
2020-04-04 12:54:39 +0530

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListAdminComponent } from './components/user-management/admin/list-admin/list-admin.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HeaderComponent},
+  {path:'admin/list',component:ListAdminComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoMaterialModule } from 'src/material-module';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AddEditAdminComponent } from './components/user-management/admin/add-edit-admin/add-edit-admin.component';
+import { ListAdminComponent } from './components/user-management/admin/list-admin/list-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AddEditAdminComponent,
+    ListAdminComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DemoMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
